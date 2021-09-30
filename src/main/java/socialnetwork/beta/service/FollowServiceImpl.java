c6433149e1311604114b09482f79257c9d6767ad
rezkmohamed <momito310399@gmail.com>
2021-09-30 09:31:29 +0200

    refactor addNewFollow's logic

--- src/main/java/socialnetwork/beta/service/FollowServiceImpl.java
+++ src/main/java/socialnetwork/beta/service/FollowServiceImpl.java
@@ -6,19 +6,24 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import socialnetwork.beta.entity.Follow;
+import socialnetwork.beta.entity.Profile;
 import socialnetwork.beta.repo.FollowRepo;
+import socialnetwork.beta.repo.ProfileRepo;
 import socialnetwork.beta.utils.FollowUtils;
-
 
 @Service
 public class FollowServiceImpl implements FollowService {
 	@Autowired
 	private FollowRepo followRepo;
+	@Autowired
+	private ProfileRepo profileRepo;
 	
 	@Override
 	@Transactional
 	public String addFollow(String idProfileFollower, String idProfileFollowed) {
-		Follow followToAdd = FollowUtils.createNewFollowFromIdProfiles(idProfileFollower, idProfileFollowed);
+		Profile profileFollower = profileRepo.findProfile(idProfileFollower);
+		Profile profileFollowing = profileRepo.findProfile(idProfileFollowed);
+		Follow followToAdd = FollowUtils.createNewFollowFromProfiles(profileFollower, profileFollowing);
 		
 		return followRepo.addFollow(followToAdd);
 	}
--- src/main/java/socialnetwork/beta/utils/FollowUtils.java
+++ src/main/java/socialnetwork/beta/utils/FollowUtils.java
@@ -3,15 +3,18 @@ package socialnetwork.beta.utils;
 import java.util.Date;
 
 import socialnetwork.beta.entity.Follow;
+import socialnetwork.beta.entity.Profile;
 
 public class FollowUtils {
 	/**
 	 * FIXME
 	 */
-	public static Follow createNewFollowFromIdProfiles(String idProfileFollower, String idProfileFollowed) {
+	public static Follow createNewFollowFromProfiles(Profile profileFollower, Profile profileFollowed) {
 		Follow follow = new Follow();
 		follow.setDate(new Date());
+		follow.setFollower(profileFollower);
+		follow.setFollowed(profileFollowed);
 		
-		return null;
+		return follow;
 	}
 }	
