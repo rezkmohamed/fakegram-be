53ab9f15b9d5d133a6ec17386ab432ce1f9b2ddb
rezkmohamed <momito310399@gmail.com>
2021-10-08 11:55:49 +0200

    refactor fetchLoggedProfile's logic

--- src/main/java/socialnetwork/beta/service/ProfileServiceImpl.java
+++ src/main/java/socialnetwork/beta/service/ProfileServiceImpl.java
@@ -7,11 +7,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.transaction.annotation.Transactional;
 
+import socialnetwork.beta.dto.PostDTO;
 import socialnetwork.beta.dto.ProfileDTO;
 import socialnetwork.beta.dto.UserDTO;
+import socialnetwork.beta.entity.Post;
 import socialnetwork.beta.entity.Profile;
+import socialnetwork.beta.repo.PostRepo;
 import socialnetwork.beta.repo.ProfileRepo;
 import socialnetwork.beta.utils.ImgUtils;
+import socialnetwork.beta.utils.PostUtils;
 import socialnetwork.beta.utils.ProfileUtils;
 
 @Service
@@ -20,6 +24,8 @@ public class ProfileServiceImpl implements ProfileService {
 //	private ImgUtils imgUtils;
 	@Autowired
 	private ProfileRepo profileRepo;
+	@Autowired
+	private PostRepo postRepo;
 
 	@Override
 	@Transactional
@@ -58,7 +64,11 @@ public class ProfileServiceImpl implements ProfileService {
 	public ProfileDTO findProfileById(String idProfile) {
 		Profile profile = profileRepo.findProfile(idProfile);
 		if(profile != null) {
-			return ProfileUtils.profileToDTO(profile);
+			List<Post> posts = postRepo.findPostsProfilePage(idProfile);
+			List<PostDTO> postsDTO = PostUtils.postToDTO(posts);
+			ProfileDTO profileDTO = ProfileUtils.profileToDTO(profile);
+			profileDTO.setPosts(postsDTO);
+			return profileDTO;
 		}
 		
 		return null;
