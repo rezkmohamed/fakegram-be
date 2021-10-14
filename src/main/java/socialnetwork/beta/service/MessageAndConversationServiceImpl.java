976711e1a19ab4d1da24c228574b071fdfc597f4
rezkmohamed <momito310399@gmail.com>
2021-10-14 12:03:37 +0200

    finished MessageAndConversationService

--- src/main/java/socialnetwork/beta/service/MessageAndConversationServiceImpl.java
+++ src/main/java/socialnetwork/beta/service/MessageAndConversationServiceImpl.java
@@ -1,6 +1,7 @@
 package socialnetwork.beta.service;
 
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -12,6 +13,7 @@ import socialnetwork.beta.dto.ConversationDTO;
 import socialnetwork.beta.dto.MessageDTO;
 import socialnetwork.beta.entity.Conversation;
 import socialnetwork.beta.entity.Message;
+import socialnetwork.beta.entity.Profile;
 import socialnetwork.beta.repo.MessageAndConversationRepo;
 import socialnetwork.beta.repo.ProfileRepo;
 import socialnetwork.beta.utils.MessageAndConversationUtils;
@@ -88,22 +90,59 @@ public class MessageAndConversationServiceImpl implements MessageAndConversation
 	@Override
 	@Transactional
 	public MessageDTO addMessage(MessageDTO message) {
-		// TODO Auto-generated method stub
-		return null;
+		Profile profile1 = profileRepo.findProfile(message.getIdProfileSender());
+		Profile profile2 = profileRepo.findProfile(message.getIdProfileReciver());
+		if(profile2 == null || profile1 == null) {
+			return null;
+		}
+		Date now = new Date(System.currentTimeMillis());
+		
+		Message msg = new Message(now, false);
+		msg.setMessage(message.getMessage());
+		msg.setProfileSender(profile1);
+		msg.setProfileReciver(profile2);
+		
+		Conversation conversation = messageAndConversationRepo.getConversation(message.getIdConversation());
+		msg.setConversation(conversation);
+		String newIdMsg = messageAndConversationRepo.addMessage(msg);
+		message.setIdMessage(newIdMsg);
+		messageAndConversationRepo.addMessage(msg);
+		
+		return message;
 	}
 
 	@Override
 	@Transactional
 	public ConversationDTO createNewConversation(String idFirstProfile, String idSecondProfile) {
-		// TODO Auto-generated method stub
-		return null;
+		Profile firstProfile = profileRepo.findProfile(idFirstProfile);
+		if(firstProfile == null) {
+			return null;
+		}
+		Profile secondProfile = profileRepo.findProfile(idSecondProfile);
+		if(secondProfile == null) {
+			return null;
+		}
+		Conversation conversation = new Conversation();
+		
+		conversation.setFirstProfile(firstProfile); 
+		conversation.setSecondProfile(secondProfile);
+		String idConversation = messageAndConversationRepo.createNewConversation(conversation);
+		ConversationDTO ris = MessageAndConversationUtils.conversationEntityToDTO(conversation);
+		ris.setIdConversation(idConversation);
+		
+		return ris;
 	}
 
 	@Override
 	@Transactional
 	public boolean setMessagesAsSeen(String idConversation, String idLoggedProfile) {
-		// TODO Auto-generated method stub
-		return false;
+		Conversation conversation = messageAndConversationRepo.getConversation(idConversation);
+		if(!conversation.getFirstProfile().getIdProfile().equals(idLoggedProfile) && 
+				!conversation.getSecondProfile().getIdProfile().equals(idLoggedProfile)) {
+			return false;
+		}
+		
+		return messageAndConversationRepo.setMessagesAsSeen(idConversation, idLoggedProfile);
 	}
 
 }
